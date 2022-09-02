6a45768578b80ab776a09359cc21cd6a2f80c43c
yigitden <yigitcandeniz@gmail.com>
2022-09-02 23:51:38 +0300

    added functions and lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,54 @@
 <template>
-   
+  <div class="main">
+    <h1>Reaction Timer Game</h1>
+    <button @click="start" :disabled="isPlaying">Play</button>
+    <Results v-if="showResult" :score="score" />
+    <BlockFrame v-if="isPlaying" :delay="delay" @end="endGame" />
+  </div>
 </template>
 
 <script>
- 
+import Results from "./components/Results.vue";
+import BlockFrame from "./components/BlockFrame.vue";
 export default {
-  name: 'App',
- 
-}
+  name: "App",
+  data() {
+    return {
+      delay: null,
+      isPlaying: false,
+      score: null,
+      showResult: false,
+    };
+  },
+  methods: {
+    start() {
+      (this.isPlaying = true), (this.delay = 2000 + Math.random() * 5000);
+      this.showResult = false;
+    },
+    endGame(time) {
+      (this.score = time), (this.isPlaying = false);
+      this.showResult = true;
+    },
+  },
+  components: { BlockFrame, Results },
+};
 </script>
 
 <style>
- 
+.main {
+  text-align: center;
+}
+button {
+  border: 0;
+  border-radius: 4px;
+  font-size: 20px;
+  color: white;
+  padding: 15px;
+  background-color: #6fedd6;
+  cursor: pointer;
+}
+button[disabled] {
+  opacity: 0.3;
+  cursor: not-allowed;
+}
 </style>
--- src/components/Results.vue
+++ src/components/Results.vue
@@ -1,13 +1,12 @@
 <template>
-  
+  <p>Reaction Time : {{ score }} ms</p>
 </template>
 
 <script>
 export default {
-
-}
+  props: ["score"],
+  name: "ResultsGame",
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
